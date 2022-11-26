46d71af7eeff451aec647c6b441104fdaf86dece
Yuliya <filatovajulia@rambler.ru>
2022-11-26 22:59:00 +0300

    add DZ

--- DZ/5.py
+++ DZ/5.py
@@ -11,4 +11,5 @@ by = float(input('Введите координаты точки b по оси y
 
 import math
 distans = math.sqrt((ax-bx)**2+(ay-by)**2)
-print(f'Растояние между точкой A до точки B = {distans}' )
+print(f"Растояние между точкой A до точки B = {format(distans,'.2f')} ")
+
